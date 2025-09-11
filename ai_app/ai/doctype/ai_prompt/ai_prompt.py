98b5cbedb1ae42d607110c7c102a663acd9be6fc
Ehsan Javaid <ehsanjavaid321@gmail.com>
2025-09-11 13:42:27 +0500

    response fields error fix

--- ai_app/ai/doctype/ai_prompt/ai_prompt.py
+++ ai_app/ai/doctype/ai_prompt/ai_prompt.py
@@ -1,17 +1,18 @@
 import frappe
 import google.generativeai as genai
 from frappe.model.document import Document
+import markdown
 
 
 class AiPrompt(Document):
     pass
 
-
 @frappe.whitelist()
 def generate_ai_text(prompt, docname):
     gemini_key = frappe.local.conf.get("gemini_api_key")
     if not gemini_key:
-        frappe.throw("Gemini API key not found in site_config.json or common_site_config.json")
+        frappe.throw(
+            "Gemini API key not found in site_config.json or common_site_config.json")
 
     genai.configure(api_key=gemini_key)
     answer = ""
@@ -28,10 +29,11 @@ def generate_ai_text(prompt, docname):
     except Exception as e:
         frappe.log_error(frappe.get_traceback(), "Gemini API Error")
         frappe.throw(f"Gemini API failed: {str(e)}")
-
+    
     try:
-        doc = frappe.get_doc("Ai Prompt", docname)
-        doc.db_set("response_text", answer)
+        answer_html = markdown.markdown(answer)
+        frappe.db.set_value("Ai Prompt", docname, "response_text",answer_html, update_modified=False)
+
     except Exception:
         frappe.log_error(frappe.get_traceback(), "Failed to update Ai Prompt Doc")
         frappe.throw("AI response generated but could not be saved to the document")
